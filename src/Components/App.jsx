f80e801d61a4cc7f50ec653a7616df629f153271
AlexBoyko5 <144361285+AlexBoyko5@users.noreply.github.com>
2024-03-28 17:22:46 +0100

    commit-13
    Upd CSS

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 // import reactLogo from './assets/react.svg';
 // import viteLogo from '/vite.svg';
-import './App.css';
+import './App.module.css';
 
 const Description = () => {
 	return (
